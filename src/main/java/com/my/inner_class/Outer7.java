cc0c9f9b8f5c83c808473b2c35ad1e759a221b1f
Jobs8w <job8w@outlook.com>
2021-05-24 08:28:00 +0800

    add

--- src/main/java/com/my/inner_class/Outer7.java
+++ src/main/java/com/my/inner_class/Outer7.java
@@ -9,18 +9,71 @@ package com.my.inner_class;
  * */
 public class Outer7 {
 
-    public void func() {
-        System.out.println("1");
+    /**
+     * 包含两个方法的HelloWorld接口
+     */
+    interface HelloWorld {
+        public void greet();
+
+        public void greetSomeone(String someone);
     }
 
-    public static void main(String[] args) {
-        Outer7 inner = new Outer7() {
-            public void func() {
-                System.out.println("2");
+    public void sayHello() {
+
+        // 1、局部类EnglishGreeting实现了HelloWorld接口
+        class EnglishGreeting implements HelloWorld {
+            String name = "world";
+
+            public void greet() {
+                greetSomeone("world");
+            }
+
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hello " + name);
+            }
+        }
+
+        HelloWorld englishGreeting = new EnglishGreeting();
+
+        // 2、匿名类实现HelloWorld接口
+        HelloWorld frenchGreeting = new HelloWorld() {
+            String name = "tout le monde";
+
+            public void greet() {
+                greetSomeone("tout le monde");
+            }
+
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Salut " + name);
             }
         };
-        inner.func();
+
+        // 3、匿名类实现HelloWorld接口
+        HelloWorld spanishGreeting = new HelloWorld() {
+            String name = "mundo";
+
+            public void greet() {
+                greetSomeone("mundo");
+            }
+
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hola, " + name);
+            }
+        };
+
+        englishGreeting.greet();
+        frenchGreeting.greetSomeone("Fred");
+        spanishGreeting.greet();
     }
+
+    public static void main(String... args) {
+        Outer7 myApp = new Outer7();
+        myApp.sayHello();
+    }
+
 }
 
 /*
