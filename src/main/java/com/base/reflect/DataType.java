8a6db90d92b5d889329ded494ff1c477dfeb1fbe
dioq <ptttqq@gmail.com>
2025-04-16 21:48:10 +0800

    save

--- src/main/java/com/base/reflect/DataType.java
+++ src/main/java/com/base/reflect/DataType.java
@@ -2,15 +2,17 @@ package com.base.reflect;
 
 public class DataType {
 
-    public static void main(String[] args) {
-
-        test1();
+    public static void main(String[] args) throws ClassNotFoundException {
+//        test1();
+        test();
     }
 
     public static void test() throws ClassNotFoundException {
         String int_class = "java.lang.Integer";
         Class<?> intClass = Class.forName(int_class);
-        Class clz = int.class;
+        System.out.println(intClass.getName());
+        Class<?> clz = int.class;
+        System.out.println(clz.getName());
     }
 
     public static void test1() {
